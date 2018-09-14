0001cfa3f05bfbc87222cf4c065bbf0859737c9b
qq877542910 <877542910@qq.com>
2018-09-14 11:50:17 +0800

    添加测试体素地形工程

---
+++ MarchingCubes/Assets/Scripts/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle
+{
+    public Vector2 e1;
+    public Vector2 e2;
+    public Vector2 e3;
+
+    public Vector3 v1;
+    public Vector3 v2;
+    public Vector3 v3;
+
+    public Vector3 worldPoint;
+
+    public Vector3 worldV1 { get { return worldPoint + v1; } }
+    public Vector3 worldV2 { get { return worldPoint + v2; } }
+    public Vector3 worldV3 { get { return worldPoint + v3; } } 
+}
